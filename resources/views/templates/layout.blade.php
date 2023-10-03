af44884690b0d2ec41e2d3260378677299755f8e
NEWUSER <hfidz7534@gmail.com>
2023-10-03 11:30:19 +0700

    sdfs

--- resources/views/templates/layout.blade.php
+++ resources/views/templates/layout.blade.php
@@ -8,11 +8,11 @@
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback" type="text/css">
   <!-- Font Awesome -->
-  {{-- <link rel="stylesheet" href="{{ asset('adminlte3') }}/plugins/fontawesome-free/css/all.min.css" type="text/css">
+  <link rel="stylesheet" href="{{ asset('adminlte3') }}/plugins/fontawesome-free/css/all.min.css" type="text/css">
   <!-- Theme style -->
   <link rel="stylesheet" href="{{ asset('adminlte3') }}/dist/css/adminlte.min.css" type="text/css">
   {{-- Sweetalert style --}}
-  {{-- <link rel="stylesheet" href="{{asset('adminlte3')}}/plugins/sweetalert2/sweetalert2.min.css" type="text/css"> --}}
+  <link rel="stylesheet" href="{{asset('adminlte3')}}/plugins/sweetalert2/sweetalert2.min.css" type="text/css">
   {{-- adminlte3 --}}
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/admin-lte@3.1/dist/css/adminlte.min.css">
 
@@ -29,7 +29,6 @@
 {{-- DataTables --}}
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/datatables/1.10.21/css/dataTables.bootstrap.min.css">
 <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css">
-
 <link rel="stylesheet" href="https://emarket.jh-beon.cloud/public/adminlte3/plugins/fontawesome-free/css/all.min.css">
 
   </head>
